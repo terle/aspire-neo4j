e2c7ffea4a852de0f37edd26bc943281bfb42616
Terkel Brix <terkel.brix@gmail.com>
2026-01-02 18:15:25 +0100

    Consolidated examples and source code for easier testing

--- src/example/AspireApp1.AppHost/Program.cs
+++ src/example/AspireApp1.AppHost/Program.cs
@@ -1,17 +1,18 @@
-using NorthernNerds.Aspire.Hosting.Neo4j;
+//using NorthernNerds.Aspire.Hosting.Neo4j;
 
 var builder = DistributedApplication.CreateBuilder(args);
 
 var neo4jPass = builder.AddParameter("neo4j-pass", secret: true);
 var neo4jUser = builder.AddParameter("neo4j-user", secret: true);
 
-var neo4jDb = builder.AddNeo4j("graph-db", neo4jUser, neo4jPass);
+//var neo4jDb = builder.AddNeo4j("graph-db", neo4jUser, neo4jPass);
 
 var apiService = builder.AddProject<Projects.AspireApp1_ApiService>("apiservice");
 
 builder.AddProject<Projects.AspireApp1_Web>("webfrontend")
     .WithExternalHttpEndpoints()
     .WithReference(apiService)
-    .WithReference(neo4jDb);
+    //.WithReference(neo4jDb)
+    ;
 
 builder.Build().Run();
